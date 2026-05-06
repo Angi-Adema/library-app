01050da78081b5961bacd956f2902cd05d182782
Angi-Adema <dr.adema@gmail.com>
2026-05-06 10:44:00 -0600

    added Inventory.java to handle library processes such as tracking book inventory, checked out books, and the processes of adding a book to inventory, checking out a book, returning a book, printing all books in the inventory, and searching by title.

---
+++ src/com/angiadema/libraryapp/Inventory.java
@@ -0,0 +1,42 @@
+package com.angiadema.libraryapp;
+
+import java.util.ArrayList;
+
+public class Inventory {
+
+	// ArrayList to store book inventory
+	ArrayList<String> bookInventory = new ArrayList<>();
+	
+	// ArrayList to store checked out books
+	ArrayList<String> checkedOutBooks = new ArrayList<>();
+	
+	// Add books to the inventory
+	public void addBook() {
+		
+	}
+	
+	// Remove book from inventory and store in checkedOutBooks
+	public void borrowBook() {
+		
+	}
+	
+	// Return book by removing from checkedOutBooks and adding back to inventory
+	public void returnBook() {
+		
+	}
+	
+	// Call printBookInfo() and iterate over book collection to print all book details
+	public void printAll() {
+		
+	}
+	
+	// Search book inventory and return matching or partially matching titles
+	public void searchByTitle(String title) {
+		
+	}
+	
+	// Add helper methods to handle edge cases (user returns book when none have been checked out)
+	public void getMainInventoryCount() {
+		
+	}
+}
