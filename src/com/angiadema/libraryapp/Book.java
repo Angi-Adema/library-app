cb743ff5ed3174b70c66e25baee784767efd2846
Angi-Adema <dr.adema@gmail.com>
2026-05-06 10:20:47 -0600

    added the Book.java file along with member variables and getter/setters and toString() method override

--- src/com/angiadema/libraryapp/Book.java
+++ src/com/angiadema/libraryapp/Book.java
@@ -1,5 +1,72 @@
 package com.angiadema.libraryapp;
 
 public class Book {
+	
+	// Member variables
+	private int id;
+	private String title;
+	private String author;
+	private String isbn;
+	private int numberOfPages;
+	
+	// Default constructor	
+	public Book() {
+		
+	}
+	
+	// Parameterized constructor	
+	public Book(int id, String title, String author, String isbn, int numberOfPages) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.numberOfPages = numberOfPages;
+	}
+
+	// Getter and Setter methods
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+	public void setNumberOfPages(int numberOfPages) {
+		this.numberOfPages = numberOfPages;
+	}
+
+	// Override the toString() method
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", author=" + author + ", isbn=" + isbn + ", numberOfPages="
+				+ numberOfPages + "]";
+	}
+	
+//	public printBookInfo() {
+//		
+//	}
+	
+	
 
 }
