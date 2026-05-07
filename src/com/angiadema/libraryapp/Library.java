98fd58f45f2a705340dab111efdcb8be81b20426
Angi-Adema <dr.adema@gmail.com>
2026-05-07 11:55:22 -0600

    added another regex next line character to a print statement in Library.java.

--- src/com/angiadema/libraryapp/Library.java
+++ src/com/angiadema/libraryapp/Library.java
@@ -118,7 +118,7 @@ public class Library {
 					
 			} catch (Exception e) {
 				// Handle the exception notifying the user and clearing buffer
-				System.out.println("Invalid input. Please try again!");
+				System.out.println("Invalid input. Please try again!\n");
 				scnr.nextLine();
 				
 			}
